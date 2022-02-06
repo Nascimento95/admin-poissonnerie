21d9c97f9d85e71bdb7169115f89f36f3fc60e99
Nascimento95 <marco-pereira@hotmail.fr>
2022-02-06 17:21:02 +0100

    page facture en cour

--- src/pages/facture.js
+++ src/pages/facture.js
@@ -8,7 +8,7 @@ function Facture() {
         <Box sx={{display:"flex"}}>
             <NavFixeLeft/>
             <Grid sm={10} sx={{border:"1px black solid",ml:2, mr:2}}>
-                <h1>salut</h1>
+                <h1>Toutes les Facture clients</h1>
             </Grid>
             
         </Box>
